4ba31a94cab13468adecae3d9fbdc3002ae66fa6
sophie <2amslekshun@gmail.com>
2018-06-28 12:41:22 -0400

    Stub out unityApp view

---
+++ client/src/views/jokL.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="unity-player"></div>
+</template>
+<script>
+export default {
+    name: 'jokL'
+}
+</script>
+
+<style scoped>
+.unity-player {
+    background: #888888;
+    margin-top: 6%;
+    width: 88%;
+    height: 88%;
+}
+</style>
