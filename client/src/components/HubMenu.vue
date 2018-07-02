fba258722c08d618ab9e474fb0e6b53cce7f24e2
funnymania <2amslekshun@gmail.com>
2018-07-02 12:06:37 -0400

    Refactor menu into component

---
+++ client/src/components/HubMenu.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="nav">
+      <router-link to="/theauxts" class="menu-a">Control Theauxts</router-link>
+      <br><br>
+      <router-link to="/jokL" class="menu-a">2 am Slekshun: jokL</router-link>   
+      <br><br>
+      <router-link to="/noSCL" class="menu-a">See in NoSCL</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HubMenu",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #060606;
+    text-decoration: none;
+    &.router-link-exact-active {
+      color: #ececec;
+    }
+    &:hover {
+      color: #ececec;
+    }
+  }
+  .menu-a {
+    display: inline-block;
+    padding-bottom: 15px;
+  }
+}
+</style>
