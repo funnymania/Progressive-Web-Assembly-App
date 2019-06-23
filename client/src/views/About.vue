0e0d048cd71ee985fd6d457154fce738b083fbfc
sophie <2amslekshun@gmail.com>
2019-06-23 12:09:40 -0700

    Add about text

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>
+      &emsp;the goal is to get people
+      <br>to stop applying for jobs obsessively
+      <br>and companies to stop posting
+      <br>jobs in the traditional fashion.
+    </p>
+    <p>
+      &emsp;by satirizing the pursuit
+      <br>as a pokemon trading card game,
+      <br>gachapon, or otherwise endless
+      <br>quest to Catch 'em ALL!
+    </p>
+    <p>
+      &emsp;All cards die fast to reflect
+      <br>the hopelessness in returned opportunities
+      <br>to recently applied for positions.
+    </p>
   </div>
 </template>
