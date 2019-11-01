761c4e55a6d43aafd412b5f81a9c1ac5b1f3c2dc
sophie <2amslekshun@gmail.com>
2019-11-01 00:40:36 -0700

    Add tests for official orgs
    Will use a binary search implementation to test if a search results match
    the search query

--- test/hirable.js
+++ test/hirable.js
@@ -60,20 +60,138 @@ function testRootUser() {
   })
 }
 
-
-
 const testOfficialOrg = () => {
-  // As another's...
-  // Adding a card from a different orgid should not be allowed.
-  // Adding a card from associated orgid should be allowed. 
-  // Searching should be allowed. 
+  const ghostsToken = uuidv4()
+  let offOrg
+
+  describe('Official Orgs', function () {
+    before(async () => {
+      // Clear relevant tables.
+      try {
+        await pgClient.query('DELETE FROM cards')
+        await pgClient.query('DELETE FROM official_rights')
+        await pgClient.query('DELETE FROM official_orgs')
+        await pgClient.query('DELETE FROM sup_orgs')
+      } catch (err) {
+        console.log(err)
+      }
+
+      // Create super user
+      try {
+        await pgClient.query(`INSERT INTO official_orgs VALUES ('${ghostsToken}', 0)`)
+        await pgClient.query(`INSERT INTO sup_orgs VALUES (0, 'Ghosts')`)
+        await pgClient.query(`INSERT INTO official_rights VALUES ('${ghostsToken}', TRUE, TRUE)`)
+      } catch (err) {
+        console.log(err)
+      }
+
+      // Add official org to test
+      try {
+        offOrg = await hirable.makeOfficial(1, 'Mozilla Corporation')
+      } catch (err) {
+        console.log(err)
+      }
+    })
+
+    describe('Add a card associated with correct org', () => {
+      it('should add the card, and search for that card', async () => {
+        try {
+          const insRes = await hirable.insertCorn(offOrg.rows[0].api_token, offOrg.rows[0].org_id, 'https://mozilla.org', 'San Francisco', 'Software Engineer')
+          const searchRes = await hirable.search({ location: insRes.rows[0].location }, 'cards')
+          assert.deepStrictEqual(insRes.rows[0], searchRes.rows[0])
+        } catch (err) {
+          assert.fail(err)
+        }
+      })
+    })
+
+    describe('Add card of a different org', () => {
+      it('should return an error message', async () => {
+        try {
+          const insRes = await hirable.insertCorn(offOrg.rows[0].api_token, offOrg.rows[0].org_id - 1, 'https://mozilla.org', 'San Francisco', 'Software Engineer')
+          assert.fail(insRes)
+        } catch (err) {
+          assert.ok(err)
+        }
+      })
+    })
+  })
 }
 
 const testPublic = () => {
-  // As public
-  // Inserting should not be allowed.
-  // Searching should be allowed. 
+  const ghostsToken = uuidv4()
+  let genUUID = uuidv4()
 
+  describe('Public usage', function () {
+    before(async () => {
+      // Clear relevant tables.
+      try {
+        await pgClient.query('DELETE FROM cards')
+        await pgClient.query('DELETE FROM official_rights')
+        await pgClient.query('DELETE FROM official_orgs')
+        await pgClient.query('DELETE FROM sup_orgs')
+      } catch (err) {
+        console.log(err)
+      }
+
+      // Create super user
+      try {
+        await pgClient.query(`INSERT INTO official_orgs VALUES ('${ghostsToken}', 0)`)
+        await pgClient.query(`INSERT INTO sup_orgs VALUES (0, 'Ghosts')`)
+        await pgClient.query(`INSERT INTO official_rights VALUES ('${ghostsToken}', TRUE, TRUE)`)
+      } catch (err) {
+        console.log(err)
+      }
+
+      // TODO: Add some cards for testing search
+    })
+
+    describe('Add a card with incorrect api_token', () => {
+      it('should return an error message', async () => {
+        try {
+          const insRes = await hirable.insertCorn(genUUID, 0, 'https://mozilla.org', 'San Francisco', 'Software Engineer')
+          assert.fail(insRes)
+        } catch (err) {
+          assert.ok(err)
+        }
+      })
+    })
+
+    describe('search for a card via orgName', () => {
+      it('should return a result matching the orgName', async () => {
+        try {
+          const searchRes = await hirable.search({ location: 'Seattle' }, 'cards')
+          assert.equal(sortAndBinarySearch(searchObj, rows[0]))
+          assert.deepStrictEqual(insRes.rows[0], searchRes.rows[0])
+        } catch (err) {
+          assert.fail(err)
+        }
+      })
+    })
+
+    describe('search for a card via many fields', () => {
+      it('should return a result matching the fields searched for', async () => {
+        try {
+          const searchObj = { location: 'Seattle', role: 'Software Engineer', org_id: 1 }
+          const { rows } = await hirable.search(searchObj, 'cards')
+          assert.strictEqual(sortAndBinarySearch(searchObj, rows[0]))
+        } catch (err) {
+          assert.fail(err)
+        }
+      })
+    })
+  })
 }
 
-connect().then(hirable.connect()).then(testRootUser())
+// TODO: Return 'true' if all properties of 'one' are found in 'other'
+function sortAndBinarySearch(one, other) {
+  one.sort()
+  other.sort()
+
+  // TODO: Binsearch implementation
+}
+
+connect().then(hirable.connect())
+  .then(testRootUser())
+  .then(testOfficialOrg())
+  .then(testPublic())
